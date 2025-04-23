6bdfdeb110a79627c585bcbfdaf49bc7dece6c0f
krahulmeena9968 <krahulmeena9968@gmail.com>
2025-04-23 18:03:40 +0530

    First commit where push all code in once

--- backend/index.js
+++ backend/index.js
@@ -153,46 +153,46 @@ app.use(bodyParser.json());
 //   res.send("Done!");
 // });
 
-app.get("/addpositions", async (req, res) => {
-  let tempPositions = [
-    {
-      product: "CNC",
-      name: "EVEREADY",
-      qty: 2,
-      avg: 316.27,
-      price: 312.35,
-      net: "+0.58%",
-      day: "-1.24%",
-      isLoss: true,
-    },
-    {
-      product: "CNC",
-      name: "JUBLFOOD",
-      qty: 1,
-      avg: 3124.75,
-      price: 3082.65,
-      net: "+10.04%",
-      day: "-1.35%",
-      isLoss: true,
-    },
-  ];
+// app.get("/addpositions", async (req, res) => {
+//   let tempPositions = [
+//     {
+//       product: "CNC",
+//       name: "EVEREADY",
+//       qty: 2,
+//       avg: 316.27,
+//       price: 312.35,
+//       net: "+0.58%",
+//       day: "-1.24%",
+//       isLoss: true,
+//     },
+//     {
+//       product: "CNC",
+//       name: "JUBLFOOD",
+//       qty: 1,
+//       avg: 3124.75,
+//       price: 3082.65,
+//       net: "+10.04%",
+//       day: "-1.35%",
+//       isLoss: true,
+//     },
+//   ];
 
-  tempPositions.forEach((item) => {
-    let newPostion = new PositionMode({
-      product: item.product,
-      name: item.name,
-      qty: item.qty,
-      avg: item.avg,
-      price: item.price,
-      net: item.net,
-      day: item.day,
-      isLoss: item.isLoss,
-    });
+//   tempPositions.forEach((item) => {
+//     let newPostion = new PositionMode({
+//       product: item.product,
+//       name: item.name,
+//       qty: item.qty,
+//       avg: item.avg,
+//       price: item.price,
+//       net: item.net,
+//       day: item.day,
+//       isLoss: item.isLoss,
+//     });
 
-    newPostion.save();
-  });
-  res.send("Done!");
-});
+//     newPostion.save();
+//   });
+//   res.send("Done!");
+// });
 
 app.get("/allholdings", async (req, res) => {
   let allHoldings = await HoldingModel.find({});
